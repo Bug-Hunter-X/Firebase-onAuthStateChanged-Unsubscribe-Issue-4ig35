21afe65c46b3c933c365849d8ede7479506b7e2f
BugHunter-X <askzenai@gmail.com>
2025-01-18 23:58:08 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,22 @@
+import { onAuthStateChanged } from "firebase/auth";
+import { useEffect } from 'react';
+
+const unsubscribe = onAuthStateChanged(auth, (user) => {
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/auth/web/user-properties
+    const uid = user.uid;
+    console.log("User is signed in:", user);
+    // ...
+  } else {
+    // User is signed out
+    // ...
+    console.log("User is signed out");
+  }  
+});
+
+useEffect(() => {
+  return () => {
+    unsubscribe(); // Unsubscribe from the listener when the component is unmounted
+  };
+}, []);
